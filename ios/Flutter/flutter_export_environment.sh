e4314a3a9d9d8cd0793cfb0ae02651189d4ac09b
Sujanlamichhane31 <sujanlamichhane742@gmail.com>
2022-11-26 13:57:38 +0545

    search and map page all done

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/aabhashrai/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/aabhashrai/Documents/flutter projects/Renterii-Mobile-Consumer"
+export "FLUTTER_ROOT=C:\src\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\Officework\Renterii-Mobile-Consumer"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=/Users/aabhashrai/Documents/flutter projects/Renterii-Mobile-Consumer/lib/main.dart"
+export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
-export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=/Users/aabhashrai/Documents/flutter projects/Renterii-Mobile-Consumer/.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
